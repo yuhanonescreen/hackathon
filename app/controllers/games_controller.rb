9afcbe8187d0bcf97335da2282846a2e9354157d
Yuhan Zhang <OFFICE\yzhang@irdev24.office.onescreeninc.com>
2013-10-24 21:29:59 -0700

    to post content_id and start_time

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -22,11 +22,14 @@ class GamesController < ApplicationController
       time_passed = Time.now.to_i - @game.created_at.to_i
       if( time_passed < @game.duration + 20 ) 
         # play the f
-        
+          @content_id = @game.content_id
+          @created_at = @game.created_at.to_i
         return
       end
     end
 
+    @content_id = 0
+    @created_at = 0
     # TODO how to pick to video?
 
     Twitter.configure do |config|
@@ -36,16 +39,6 @@ class GamesController < ApplicationController
     
     response = Twitter.client.search("sport", :count=>100, :result_type => 'recent')
     @text = response.results.collect {|t| t.text}.flatten
-      content_id = 5153210
-      
-      # create a new game  
-      @game = Game.new
-      @game.content_id = content_id
-      content = OneScreen::Internal::Content.find(content_id)
-      asset_id = content.preview_asset_id
-      asset = OneScreen::Internal::Asset.find(asset_id)
-      @game.duration = asset.duration / 1000
-      @game.save!
   end
   
   def create_game
